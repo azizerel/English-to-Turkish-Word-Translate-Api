ece96fa5eb0bf1746530b4ace1e9ebd6007e17c1
Abdulaziz Erel <azizerel69@gmail.com>
2020-06-09 22:24:06 +0300

    initial commit

---
+++ src/main/java/com/translate/wordapp/WordappApplication.java
@@ -0,0 +1,13 @@
+package com.translate.wordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/translate/wordapp/WordappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translate.wordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
