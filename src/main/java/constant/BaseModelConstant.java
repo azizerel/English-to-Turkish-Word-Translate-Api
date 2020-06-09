54aefe53b300ecf43dbc4edf0f765786cf56cf34
Abdulaziz Erel <azizerel69@gmail.com>
2020-06-09 23:21:35 +0300

    BaseModelConstant created

---
+++ src/main/java/constant/BaseModelConstant.java
@@ -0,0 +1,15 @@
+package constant;
+
+/**
+ * created by Abdulaziz Erel on 22:48 09.06.2020
+ **/
+public final class BaseModelConstant {
+
+    public static final String ID_COLUMN_NAME = "ID";
+    public static final String CREATED_DATE_COLUMN_NAME = "CREATED_DATE";
+    public static final String CREATED_BY_COLUMN_NAME = "CREATED_BY";
+
+    private BaseModelConstant() {
+        //can not create instance
+    }
+}
